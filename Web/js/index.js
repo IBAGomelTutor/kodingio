cb32fe9a1d4c15ca189c376c928e6054143ad15d
dpilipenka <dpilipenka@users.noreply.github.com>
2016-06-28 12:40:53 +0300

    updated test
    updated

--- Web/js/index.js
+++ Web/js/index.js
@@ -7,6 +7,6 @@ handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
 handle["/show"] = requestHandlers.show;
-;
+handle["/test"] = requestHandlers.test;
 
 server.start(router.route, handle);
